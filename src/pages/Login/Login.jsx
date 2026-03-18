0285e2d2e31bd3284b1f8e55fccbd76378cab9d9
Lakshya <lxkshya@gmail.com>
2026-03-18 23:45:47 +0530

    front-end of login page done

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -6,8 +6,20 @@ const Login = () => {
     <div className='login'>
         <img src={logo} className='login-logo' alt = ""/>
         <div className='login-form'>
-            <h1>Sign In</h1>
-            <form action=""></form>
+            <h1>Sign Up</h1>
+            <form>
+                <input type='text' placeholder='Your Name'/>
+                <input type='email' placeholder='your@email.com'/>
+                <input type='password' placeholder='Password'/>
+                <button>Sign Up</button>
+                <div className='form-help'>
+                    <div className='remember'>
+                        <input type='checkbox'/>
+                        <label htmlFor=''>Remember Me</label>
+                    </div>
+                    <p>Need Help?</p>
+                </div>
+            </form>
         </div>
     </div>
   )
