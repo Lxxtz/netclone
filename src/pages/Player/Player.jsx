9c0e15476da55605fdccf7ad83a3c6759ba83186
Lakshya <lxkshya@gmail.com>
2026-03-24 21:52:45 +0530

    for local reference

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -5,7 +5,7 @@ const Player = () => {
   return (
     <div className='player'>
       <img src={back_arrow_icon} alt="" />
-      <iframe width='90%' height='90%' src='https://www.youtube.com/embed/YQQD67N5pi0&t=415s' title='trailer' frameBorder='0' allowFullScreen></iframe>
+      <iframe width='90%' height='90%' src='https://www.youtube.com/embed/YQQD67N5pi0' title='trailer' frameBorder='0' allowFullScreen></iframe>
     <div className="player-info">
       <p>Published Date</p>
       <p>Name</p>
